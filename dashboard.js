c7eafa95f3f546d537fae2619e3f180ec64936d3
SanchitaK-11 <sanchita.kengale25@pccoepune.org>
2026-05-17 01:53:42 +0530

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,10 @@
+// Redirect function for cards
+function openPage(page){
+    window.location.href = page;
+}
+
+// Logout function (example)
+function logout(){
+    alert("Logged out successfully!");
+    window.location.href = "index.html"; // login page
+}
